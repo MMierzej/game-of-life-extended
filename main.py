a01f2f89720c2b121e8370847e0e26f935bf9149
Marcin Mierzejewski <322680@uwr.edu.pl>
2020-12-17 00:01:10 +0100

    main.py: added loop to run the game, small change to next_state

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import copy as cp
 import random
+import time
 
 
 DIRS = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
 
-def generate(h, w):
-    board = [[random.randint(0, 1) for i in range(w)] for j in range(h)]
+def generate(height, width):
+    board = [[random.randint(0, 1) for i in range(width)] for j in range(height)]
     return board
 
 def draw(plansza):  # funkcja wypisująca planszę do konsoli
@@ -18,6 +19,7 @@ def draw(plansza):  # funkcja wypisująca planszę do konsoli
 
         print()
 
+# "funkcja prywatna" funkcji next_state
 def count_neighbours(field, x, y, lx, ly):
     counter = 0
 
@@ -29,8 +31,15 @@ def count_neighbours(field, x, y, lx, ly):
 
     return counter
 
-def next_state(prev):
-    result = cp.deepcopy(prev)  # next state based on previous state
+def clone_board(output_board, input_board):
+    """ copies the content of prev to board """
+    for i in range(len(prev)):
+        for j in range(len(prev[0])):
+            output_board[i][j] = input_board[i][j]
+
+def next_state(result, prev):
+    # prev - lista list, poprzednia plansza
+    # result - następny stan modyfikacja "w miejscu"
 
     ly = len(prev)
     lx = len(prev[0])
@@ -38,7 +47,6 @@ def next_state(prev):
     for y in range(ly):
         for x in range(lx):
             neighbours = count_neighbours(prev, x, y, lx, ly)
-
             if neighbours == 3:
                 result[y][x] = 1 
             elif prev[y][x] == 1 and neighbours == 2:
@@ -46,10 +54,42 @@ def next_state(prev):
             else:
                 result[y][x] = 0
 
-    return result
+def modify(plansza, x, y):
+    if plansza[y][x] == 1:
+        plansza[y][x] = 0
+    elif plansza[y][x] == 0:
+        plansza[y][x] = 1
 
-def modify(plansza, x, y): # zmienia stan komórki
-    if plansza[x][y] == 1:
-        plansza[x][y] = 0
-    elif plansza[x][y] == 0:
-        plansza[x][y] = 1
+if __name__ == "__main__":
+
+    txt = """
+    0000000000000000000000
+    0000000000000000000000
+    0000000000000011100000
+    0000000000000000000000
+    0000000000000000000000
+    0000000000000000000000
+    0000000000000000000000
+    0011100000000000000000
+    0000100000000000000000
+    0001000000000000000000
+    0000000000000000000000
+    """
+
+    height = 15
+    width = 10
+    # board = generate(height, width)
+    board = [ [ int(c) for c in wiersz ] for wiersz in txt.split() ]
+    prev = cp.deepcopy(board)
+    counter = 0
+    interval = 1
+    
+    while counter < 150:
+        if counter % interval == 0:
+            draw(board)
+            print("\n*****************************\n")
+            time.sleep(0.1)
+
+        clone_board(prev, board)  # ta funkcja modyfikuje prev, zachowujemy tu stan planszy przed nową iteracją
+        next_state(board, prev)   # ta funkcja generuje następny stan planszy (do zmiennej board)
+        counter += 1
--- to-do.md
+++ to-do.md
@@ -1,3 +1,26 @@
+# Tasks (zatwierdzone i do zrobienia)
+
+* [piątek 18.12., @all] przygotowanie propozycji rodzajów żywych komórek i interakcji (3 rodzaje i interakcje: 1 - 2, 1 - 3, 2 - 3)
+
+* [18.12., Mierzej] więcej rodzajów jednostek - dopisanie do generatora tych nowych jednostek (numery 1, 2, 3)
+
+* [18.12., Andrzej] drukowanie planszy z większą liczbą rodzajów jednostek (1 - 'o', 2 - 'x', 3 - '@')
+
+* [18.12., Michał] początek GUI (tkinter): przy odpaleniu programu wyskakuje puste okno (z przyciskami ewentualnie) < gui.py
+
+## Propozycje (dalsze kroki itd.)
+
+* [, Mierzej] jak już będzie więcej rodzajów komórek, to do poprawy ewaluacja sąsiadów
+* [, Wojtek] testy oprogramowania (pytest)
+* [, ] ujednolicenie komentarzy i nazewnictwa (jeden styl i jeden język)
+* [, ] możliwość zakończenia symulacji, jeśli plansza się zapętli
+
+## Niezwiązane z kodem
+
+* zaznajomienie się z tkinter
+* gtk?
+* GitKraken (?)
+
 ## Zrobione
 
 * jak podzielić projekt na pliki
@@ -10,21 +33,8 @@
     - 'o' dla zajętego pola przez żywą komórkę
 * [14.12.2020, Michał Mróz] funkcja, która przyjmuje planszę, koordynaty i pod podanymi współrzędnymi zmienia stan pola (żywy > martwy / martwy > żywy): modify(plansza, x, y)
 
-# Tasks (zatwierdzone i do zrobienia)
-
-* [piątek 18.12., @all] przygotowanie propozycji rodzajów żywych komórek i interakcji (3 rodzaje i interakcje: 1 - 2, 1 - 3, 2 - 3)
-* [18.12., Mierzej] więcej rodzajów jednostek - dopisanie do generatora tych nowych jednostek (numery 1, 2, 3)
-* [18.12., Andrzej] drukowanie planszy z większą liczbą rodzajów jednostek (1 - 'o', 2 - 'x', 3 - '@')
 * [18.12., Wojtek] pętla o funkcjonalności wyświetlania zmian (pętla z dobrą kolejnością już napisanych funkcji)
+
 * [18.12., Wojtek] wyświetlanie zmian co określoną liczbę pokoleń (póki co - wpisywanie przed symulacją, później (kiedy będzie GUI) - w trakcie trwania)
-* [18.12., Michał] początek GUI (tkinter): przy odpaleniu programu wyskakuje puste okno (z przyciskami ewentualnie) < gui.py
+
 * [18.12., Patryk] jakaś testowa plansza w formie txt (przekształcana na listę list), żeby dało się sprawdzać, czy ewolucja działa poprawnie
-
-## Propozycje (dalsze kroki itd.)
-
-* [18.12., Mierzej] jak już będzie więcej rodzajów komórek, to do poprawy ewaluacja sąsiadów
-
-## Niezwiązane z kodem
-
-* zaznajomienie się z tkinter
-* GitKraken (?)
