877e417844d4f2eec4f21576fdb73e2061936038
Michal <michamroz77@gmail.com>
2020-12-27 14:13:41 +0100

    modyfikacja pliku gui.py (dodanie rysowania planszy)

--- gui.py
+++ gui.py
@@ -1,43 +1,46 @@
 from tkinter import *
 
+# zmienne wielkości okna
+x = 20
+y = 20
+length = y * 30
+width = x * 30
+
 # ustawienia root
 root = Tk()
 root.title("Gra w życie")
 root.iconbitmap("icon.ico")
+resolution = str(width) + 'x' + str(length + 86)
+root.geometry(resolution)
 
-# testowa mapa
-mapa = [['_' for _ in range(10)] for _ in range(10)]
+# ustawienia canvas
+canvas = Canvas(root, width=1000, height=1000)
+canvas.place(x=0, y=30)
 
+# rysuje planszę
+def draw():
+    for x_1 in range(x):
+        for y_1 in range(y):
+            canvas.create_rectangle(30 * x_1, 30 * y_1, 20 + 30 * x_1, 20 + 30 * y_1, fill='red')
 
-# Rysuje planszę
-def rysuj():
-    for x in range(10):
-        for y in range(10):
-            label = Label(root, text=mapa[x][y])
-            label.grid(row=x, column=y + 3)
-
-
-# Podmienia Rozpocznij na Wstrzymaj | Wstrzymaj na Rozpocznij / Rozpoczęcie automatycznego aktualizowania się mapy
+# rozpoczyna symulacje
 def start():
-    rysuj()
-    button_start.grid_forget()
-    button_stop.grid(row=2, column=0)
-
+    draw()
+    button_start.place_forget()
+    button_stop.place(x=0, y=length + 55)
 
 def stop():
-    button_stop.grid_forget()
-    button_start.grid(row=2, column=0)
-
+    button_stop.place_forget()
+    button_start.place(x=0, y=length + 55)
 
 def next(a):
     if a == 1:
-        rysuj()
+        draw()
     # else:
     #     for i in range(a):
     #         rysuj()
 
-
-# Utworzone przyciski
+# utworzone przyciski
 button_reset = Button(root, text="Przywróć do stanu początkowego")
 button_clear = Button(root, text="Wyczyść")
 
@@ -47,17 +50,14 @@ button_next_10 = Button(root, text="10 ruchów", command=lambda: next(10))
 
 button_start = Button(root, text="Rozpocznij", command=start)
 button_stop = Button(root, text="Wstrzymaj", command=stop)
-# Wypisanie przycisków
-button_reset.grid(row=0, column=0, columnspan=2)
-button_clear.grid(row=0, column=2)
 
-button_next.grid(row=1, column=0)
-button_next_5.grid(row=1, column=1)
-button_next_10.grid(row=1, column=2)
+# wypisanie przycisków do okna
+button_reset.place(x=0, y=0)
+button_clear.place(x=189, y=0)
 
-button_start.grid(row=2, column=0)
-
-# Rysowanie mapki
-
+button_next.place(x=0, y=length + 25)
+button_next_5.place(x=69, y=length + 25)
+button_next_10.place(x=130, y=length + 25)
+button_start.place(x=0, y=length + 55)
 
 root.mainloop()
