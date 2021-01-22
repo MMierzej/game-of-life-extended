01ecd5c91cf7d3a91584d06d3b76571a32f7d737
Marcin Mierzejewski <322680@uwr.edu.pl>
2021-01-22 19:09:06 +0100

    dodano layout, poprawki w logice

--- gui.py
+++ gui.py
@@ -1,46 +1,65 @@
+#!/usr/bin/env python3
 from tkinter import *
 from logic import *
+import platform
+
+
+# print(platform.platform())
+
+LIFE = [2, 4, 6]
+NB = [4, 3, 3]
+SPAWN = [1, 3, 6]
+QUAKE = 1
+MUT = 0
+REP = 4
+TEMPO = 8
 
 # zmienne wielkości okna
 x = 10
 y = 10
-length = y * 30
+height = y * 30
 width = x * 30
+fwidth = width + 100 if width > 600 else 700
+fheight = height + 400
+
 counter = 0
 board = [[[0, 0] for i in range(x)] for j in range(y)]
 prev = cp.deepcopy(board)
-board_d = {}
-neighbours_d = {}
+board_d = dd(lambda : 0)
+neighbours_d = dd(lambda : 0)
 run = True
 
 # ustawienia root
 root = Tk()
 root.title("Gra w życie")
 # root.iconbitmap("icon.ico")
-resolution = str(width + 275) + 'x' + str(length + 20)
+resolution = str(fwidth) + 'x' + str(fheight)
+root.minsize(400, 400)
 root.geometry(resolution)
 root.resizable(False, False)
 
 # ustawienia canvas
-frame_board = Canvas(root, width=1000, height=1000)
-frame_board.place(x=260, y=0)
-frame_board.create_rectangle(5, 5, length + 10, width + 10)
+frame_board = Canvas(root, width=width + 10, height=height + 10)
+frame_board.place(x=fwidth // 2 - width // 2 - 10, y=60)
+frame_board.create_rectangle(5, 5, width + 10, height + 10)
 
-board_gui = Canvas(root, width=width - 5, height=length - 5)
-board_gui.place(x=270, y=10)
+board_gui = Canvas(root, width=width - 5, height=height - 5)
+board_gui.place(x=fwidth // 2 - width // 2, y=70)
 
-frame = Canvas(root, width=255, height=505)
-frame.place(x=0, y=0)
-frame.create_rectangle(5, 5, 255, 505)
+frame = Canvas(root, width=fwidth - 10, height=40)
+frame.place(x=5, y=10)
+frame.create_rectangle(fwidth // 2 - 180, 5, fwidth // 2 + 177, 40)
+
+frame_controls = Canvas(root, width = fwidth - 10, height=400)
+frame_controls.place(x=5, y=70 + height + 10)
+frame_controls.create_rectangle(5, 5, fwidth - 15, 310)
 
 
 def clicked_left(event):
     x_click = event.x // 30
     y_click = event.y // 30
-    
-    board[x_click][y_click][0] += 1
-    if board[x_click][y_click][0] == 4:
-        board[x_click][y_click][0] = 0
+
+    board[y_click][x_click][0] = (board[y_click][x_click][0] + 1) % 4
     
     draw_board()
 
@@ -48,44 +67,59 @@ def clicked_right(event):
     x_click = event.x // 30
     y_click = event.y // 30
 
-    board[x_click][y_click][0] -= 1
-    if board[x_click][y_click][0] == -1:
-        board[x_click][y_click][0] = 3
+    t = board[y_click][x_click][0]
+    if t == 1:
+        board[y_click][x_click][1] = (1 + board[y_click][x_click][1]) % LIFE[0]
+    elif t == 2:
+        board[y_click][x_click][1] = (1 + board[y_click][x_click][1]) % LIFE[1]
+    elif t == 3:
+        board[y_click][x_click][1] = (1 + board[y_click][x_click][1]) % LIFE[2]
+
+    if board[y_click][x_click][1] == 0:
+        board[y_click][x_click][0] = 0
     
     draw_board()
 
 def draw_board():
     """rysuje planszę"""
 
-    for x_1 in range(x):
-        for y_1 in range(y):
-            if board[x_1][y_1][0] == 3:
+    for y_1 in range(y):
+        for x_1 in range(x):
+            if board[y_1][x_1][0] == 3:
                 board_gui.create_rectangle(30 * x_1 + 3, 30 * y_1 + 3, 20 + 30 * x_1 + 3, 20 + 30 * y_1 + 3,
                                            fill='#ef7c27',
-                                           tags=f'{x_1},{y_1}')
-            elif board[x_1][y_1][0] == 2:
+                                           tags=f'{y_1},{x_1}')
+            elif board[y_1][x_1][0] == 2:
                 board_gui.create_rectangle(30 * x_1 + 3, 30 * y_1 + 3, 20 + 30 * x_1 + 3, 20 + 30 * y_1 + 3,
                                            fill='#009C11',
-                                           tags=f'{x_1},{y_1}')
-            elif board[x_1][y_1][0] == 1:
+                                           tags=f'{y_1},{x_1}')
+            elif board[y_1][x_1][0] == 1:
                 board_gui.create_rectangle(30 * x_1 + 3, 30 * y_1 + 3, 20 + 30 * x_1 + 3, 20 + 30 * y_1 + 3,
-                                           fill='#43b0bd',
-                                           tags=f'{x_1},{y_1}')
+                                           fill='#6699ff',
+                                           tags=f'{y_1},{x_1}')
             else:
                 board_gui.create_rectangle(30 * x_1 + 3, 30 * y_1 + 3, 20 + 30 * x_1 + 3, 20 + 30 * y_1 + 3,
                                            fill='#e3e3e3',
-                                           tags=f'{x_1},{y_1}')
-            board_gui.tag_bind(f'{x_1},{y_1}', '<Button-1>', clicked_left)
-            board_gui.tag_bind(f'{x_1},{y_1}', '<Button-3>', clicked_right)
+                                           tags=f'{y_1},{x_1}')
+            board_gui.tag_bind(f'{y_1},{x_1}', '<Button-1>', clicked_left)
+            board_gui.tag_bind(f'{y_1},{x_1}', '<Button-3>', clicked_right)
     
     board_gui.update()
 
 
 def clear():
+    global board_d
+    global neighbours_d
+    global counter
+
     for x_1 in range(x):
         for y_1 in range(y):
-            board[x_1][y_1][0] = 0
-            board[x_1][y_1][1] = 0
+            board[y_1][x_1][0] = 0
+            board[y_1][x_1][1] = 0
+    
+    board_d = dd(lambda: 0)
+    neighbours_d = dd(lambda: 0)
+    counter = 0
 
     draw_board()
 
@@ -98,7 +132,7 @@ def new_board():
 
     global counter
 
-    board = generate(x, y)
+    board = generate(x, y, LIFE, SPAWN)
     prev = cp.deepcopy(board)
 
     board_d = dd(lambda: 0)
@@ -111,7 +145,7 @@ def new_board():
 def start():
     """rozpoczyna symulacje"""
     button_start.place_forget()
-    button_stop.place(x=7, y=123)
+    button_stop.place(x=fwidth // 2 - 175, y=7)
 
     global run
     run = True
@@ -124,38 +158,146 @@ def stop():
     run = False
 
     button_stop.place_forget()
-    button_start.place(x=7, y=123)
+    button_start.place(x=fwidth // 2 - 175, y=7)
 
 def step(a):
     global counter
+    i = 0
 
-    for _ in range(a):
+    while i < a and not repetition(board_d, board, REP):
         clone_board(prev, board)  # ta funkcja modyfikuje prev, zachowujemy tu stan planszy przed nową iteracją
-        next_state(board, prev, neighbours_d, counter)  # ta funkcja generuje następny stan planszy (do zmiennej board)
+        next_state(board, prev, neighbours_d, counter, LIFE, NB, SPAWN, QUAKE, MUT)  # ta funkcja generuje następny stan planszy (do zmiennej board)
         counter += 1
         draw_board()
-        time.sleep(0.1)
+        time.sleep(1 - (TEMPO - 1) / 10)
+        i += 1
+
+def set_life(value, index):
+    LIFE[index] = int(value)
+
+def set_nb(value, index):
+    NB[index] = int(value)
+
+def set_tempo(val):
+    global TEMPO
+
+    TEMPO = int(val)
+
+def set_sub_it(val):
+    global QUAKE
+
+    QUAKE = int(val)
+
+def set_rand_it(val):
+    global MUT
+
+    MUT = int(val)
+
+def set_spawn(val, index):
+    global SPAWN
+
+    SPAWN[index] = int(val)
 
 # utworzone przyciski
-button_reset = Button(root, text="Generuj nową planszę", command=new_board)
-button_clear = Button(root, text="Wyczyść", command=clear)
+button_generate = Button(frame, width=10, text="Generuj", command=new_board)
+button_clear = Button(frame, width=10, text="Wyczyść", command=clear)
+button_start = Button(frame, width=10, text="Rozpocznij", command=start)
+button_stop = Button(frame, width=10, text="Wstrzymaj", command=stop)
 
-button_next = Button(root, text="Jeden ruch", command=lambda: step(1))
-button_next_5 = Button(root, text="5 ruchów", command=lambda: step(5))
-button_next_10 = Button(root, text="10 ruchów", command=lambda: step(10))
-
-button_start = Button(root, text="Rozpocznij", command=start)
-button_stop = Button(root, text="Wstrzymaj", command=stop)
+button_next = Button(frame_controls, width=10, text="1 ruch", command=lambda: step(1))
+button_next_5 = Button(frame_controls, width=10, text="5 ruchów", command=lambda: step(5))
+button_next_10 = Button(frame_controls, width=10, text="10 ruchów", command=lambda: step(10))
 
 # wypisanie przycisków do okna
-button_reset.place(x=7, y=7)
-button_clear.place(x=197, y=7)
+button_generate.place(x=fwidth // 2 - 55, y=7)
+button_start.place(x=fwidth // 2 - 175, y=7)
+button_clear.place(x=fwidth // 2 + 65, y=7)
 
-button_next.place(x=7, y=65)
-button_next_5.place(x=76, y=65)
-button_next_10.place(x=137, y=65)
-button_start.place(x=7, y=123)
+button_next.place(x=fwidth // 2 - 175, y=15)
+button_next_5.place(x=fwidth // 2 - 55, y=15)
+button_next_10.place(x=fwidth // 2 + 65, y=15)
 
+sl_life_1 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=10, command=lambda x : set_life(x, 0))
+sl_life_1.set(LIFE[0])
+sl_life_1.place(x=60, y=95)
+
+sl_life_2 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=10, command=lambda x : set_life(x, 1))
+sl_life_2.set(LIFE[1])
+sl_life_2.place(x=60, y=145)
+
+sl_life_3 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=10, command=lambda x : set_life(x, 2))
+sl_life_3.set(LIFE[2])
+sl_life_3.place(x=60, y=195)
+
+lab_1 = Label(frame_controls, text='1:', font=('courier new', 15))
+lab_1.place(x=20, y=113)
+lab_2 = Label(frame_controls, text='2:', font=('courier new', 15))
+lab_2.place(x=20, y=163)
+lab_3 = Label(frame_controls, text='3:', font=('courier new', 15))
+lab_3.place(x=20, y=213)
+
+lab_life = Label(frame_controls, text='Punkty życia:', font=('courier new', 13))
+lab_life.place(x=27, y=67)
+
+
+
+sl_nb_1 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=8, command=lambda x : set_nb(x, 0))
+sl_nb_1.set(NB[0])
+sl_nb_1.place(x=190, y=95)
+
+sl_nb_2 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=8, command=lambda x : set_nb(x, 1))
+sl_nb_2.set(NB[1])
+sl_nb_2.place(x=190, y=145)
+
+sl_nb_3 = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=8, command=lambda x : set_nb(x, 2))
+sl_nb_3.set(NB[2])
+sl_nb_3.place(x=190, y=195)
+
+lab_nb = Label(frame_controls, text='Sąsiedzi:', font=('courier new', 13))
+lab_nb.place(x=195, y=67)
+
+
+
+sl_sf_1 = Scale(frame_controls, orient=HORIZONTAL, from_=0, to=10, command=lambda x : set_spawn(x, 0))
+sl_sf_1.set(SPAWN[0])
+sl_sf_1.place(x=320, y=95)
+
+sl_sf_2 = Scale(frame_controls, orient=HORIZONTAL, from_=0, to=10, command=lambda x : set_spawn(x, 1))
+sl_sf_2.set(SPAWN[1])
+sl_sf_2.place(x=320, y=145)
+
+sl_sf_3 = Scale(frame_controls, orient=HORIZONTAL, from_=0, to=10, command=lambda x : set_spawn(x, 2))
+sl_sf_3.set(SPAWN[2])
+sl_sf_3.place(x=320, y=195)
+
+lab_sf = Label(frame_controls, text='Reprodukcja:', font=('courier new', 13))
+lab_sf.place(x=310, y=67)
+
+
+
+lab_ctrl = Label(frame_controls, text='Rozgrywka:', font=('courier new', 13))
+lab_ctrl.place(x=515, y=67)
+
+lab_tempo = Label(frame_controls, text='Tempo:', font=('courier new', 13))
+lab_tempo.place(x=470, y=114)
+
+lab_eq = Label(frame_controls, text='Quake:', font=('courier new', 13))
+lab_eq.place(x=470, y=164)
+
+lab_mut = Label(frame_controls, text='Mutacje:', font=('courier new', 13))
+lab_mut.place(x=450, y=214)
+
+sl_tempo = Scale(frame_controls, orient=HORIZONTAL, from_=1, to=10, command=lambda x : set_tempo(x))
+sl_tempo.set(TEMPO)
+sl_tempo.place(x=545, y=95)
+
+sl_eq = Scale(frame_controls, orient=HORIZONTAL, from_=0, to=10, command=lambda x : set_sub_it(x))
+sl_eq.set(QUAKE)
+sl_eq.place(x=545, y=145)
+
+sl_mut = Scale(frame_controls, orient=HORIZONTAL, from_=0, to=10, command=lambda x : set_rand_it(x))
+sl_mut.set(MUT)
+sl_mut.place(x=545, y=195)
 
 new_board()
 clear()
--- logic.py
+++ logic.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import copy as cp
 import random
 import time
@@ -5,35 +6,10 @@ from collections import defaultdict as dd
 
 
 """ stałe """
-HEIGHT = 10
-WIDTH = 10
 DIRS = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
-LIFE_1 = 2
-LIFE_2 = 4
-LIFE_3 = 6
-NB_1 = 4
-NB_2 = 3
-NB_3 = 3
-SUB_IT = 4
-RAND_IT = 4
-REFRESH_IT = 1
-REPS = 4
 
 
-def set_cell(board, x, y, t):
-    board[y][x][0] = t
-    
-    if t == 0:
-        board[y][x][1] = 0
-    elif t == 1:
-        board[y][x][1] = LIFE_1
-    elif t == 2:
-        board[y][x][1] = LIFE_2
-    elif t == 3:
-        board[y][x][1] = LIFE_3
-
-
-def generate(height, width):
+def generate(width, height, LIFE, SPAWN):
     """generuje losową planszę z komorkami"""
 
     # board[wiersz][kolumna][0] - gatunek
@@ -44,27 +20,23 @@ def generate(height, width):
         for j in range(width):
             p = random.uniform(0.0, 1.0)
 
-            if p < 0.7:
-                # puste pole
-                board[i][j][0] = 0
-                board[i][j][1] = 0
-            elif 0.7 <= p < 0.8:
+            if 0.7 <= p < 0.8 and SPAWN[0] != 0:
                 # gatunek 1
                 board[i][j][0] = 1
-                board[i][j][1] = LIFE_1
-            elif 0.8 <= p < 0.9:
+                board[i][j][1] = LIFE[0]
+            elif 0.8 <= p < 0.9 and SPAWN[1] != 0:
                 # gatunek 2
                 board[i][j][0] = 2
-                board[i][j][1] = LIFE_2
-            else:
+                board[i][j][1] = LIFE[1]
+            elif 0.9 <= p and SPAWN[2] != 0:
                 # gatunek 3
                 board[i][j][0] = 3
-                board[i][j][1] = LIFE_3
+                board[i][j][1] = LIFE[2]
 
     return board
 
 
-def repetition(board_d, board):
+def repetition(board_d, board, REPS):
     """ sprawdza czy plansza się zapętla """
     string = str(board)
     board_d[string] += 1
@@ -75,31 +47,14 @@ def repetition(board_d, board):
     return False
 
 
-"""
-def draw(board):  
-    # funkcja wypisująca planszę do konsoli
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j][0] == 0:
-                print(' ', end="")
-            elif board[i][j][0] == 1:
-                print('o', end="")
-            elif board[i][j][0] == 2:
-                print('x', end="")
-            elif board[i][j][0] == 3:
-                print('@', end="")
-        print()
-"""
-
-
 def count(board):
     """Funkcja zliczająca powtórzenia komórek"""
     c_1 = 0
     c_2 = 0
     c_3 = 0
 
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             if board[i][j][0] == 1:
                 c_1 += 1
             elif board[i][j][0] == 2:
@@ -134,8 +89,8 @@ def count_neighbours(board, neighbours_d, x, y):
         neighbours_d[i] = 0
 
     for dx, dy in DIRS:
-        nx = (x + dx) % WIDTH
-        ny = (y + dy) % HEIGHT
+        nx = (x + dx) % len(board[0])
+        ny = (y + dy) % len(board)
         neighbours_d[board[ny][nx][0]] += 1
 
     return neighbours_d
@@ -143,59 +98,59 @@ def count_neighbours(board, neighbours_d, x, y):
 
 def clone_board(output_board, input_board):
     """ kopiowanie zawartości input do output """
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
+    for i in range(len(input_board)):
+        for j in range(len(input_board[0])):
             output_board[i][j][0] = input_board[i][j][0]
             output_board[i][j][1] = input_board[i][j][1]
 
 
-def next_state(result, prev, neighbours_d, counter):
+def next_state(result, prev, neighbours_d, counter, LIFE, NB, SPAWN, SUB_IT, RAND_IT):
     """
     prev - lista list, poprzednia plansza
     result - następny stan modyfikacja "w miejscu"
     """
 
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
+    for y in range(len(prev)):
+        for x in range(len(prev[0])):
             cell_type = prev[y][x][0]
             nb_d = count_neighbours(prev, neighbours_d, x, y)
 
+            if SUB_IT != 0 and counter % SUB_IT == SUB_IT - 1:
+                result[y][x][1] -= 1
+
             if cell_type == 0:
-                if nb_d[1] >= NB_1 and nb_d[3] < NB_3:
+                if SPAWN[0] != 0 and nb_d[1] >= NB[0] and nb_d[3] < NB[2] and counter % SPAWN[0] == SPAWN[0] - 1:
                     result[y][x][0] = 1
-                    result[y][x][1] = LIFE_1
+                    result[y][x][1] = LIFE[0]
 
-                elif nb_d[1] < NB_1 and nb_d[2] >= NB_2 and counter % 3 == 2:
+                elif SPAWN[1] != 0 and nb_d[1] < NB[0] and nb_d[2] >= NB[1] and counter % SPAWN[1] == SPAWN[1] - 1:
                     result[y][x][0] = 2
-                    result[y][x][1] = LIFE_2
+                    result[y][x][1] = LIFE[1]
 
-                elif nb_d[2] < NB_2 and nb_d[3] >= NB_3 and counter % 6 == 5:
+                elif SPAWN[2] != 0 and nb_d[2] < NB[1] and nb_d[3] >= NB[2] and counter % SPAWN[2] == SPAWN[2] - 1:
                     result[y][x][0] = 3
-                    result[y][x][1] = LIFE_3
+                    result[y][x][1] = LIFE[2]
 
             elif cell_type == 1:
                 if nb_d[3] > 0:
                     result[y][x][1] -= 1
 
                 if nb_d[2] > 0:
-                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE_1 else LIFE_1
+                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE[0] else LIFE[0]
 
             elif cell_type == 2:
                 if nb_d[1] > 0:
                     result[y][x][1] -= 1
 
                 if nb_d[3] > 0:
-                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE_2 else LIFE_2
+                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE[1] else LIFE[1]
 
             elif cell_type == 3:
                 if nb_d[2] > 0:
                     result[y][x][1] -= 1
 
                 if nb_d[1] > 0:
-                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE_3 else LIFE_3
-
-            if counter % SUB_IT == SUB_IT - 1:
-                result[y][x][1] -= 1
+                    result[y][x][1] = prev[y][x][1] + 1 if prev[y][x][1] < LIFE[2] else LIFE[2]
 
             # eliminacja zdechłych
             if result[y][x][1] <= 0:
@@ -203,9 +158,9 @@ def next_state(result, prev, neighbours_d, counter):
                 result[y][x][1] = 0
 
     """ generowanie mutacji losowej komórki """
-    if counter % RAND_IT == RAND_IT - 1:
-        rx = random.randint(0, WIDTH - 1)
-        ry = random.randint(0, HEIGHT - 1)
+    if RAND_IT != 0 and counter % RAND_IT == RAND_IT - 1:
+        rx = random.randint(0, len(prev[0]) - 1)
+        ry = random.randint(0, len(prev) - 1)
         t = random.randint(0, 3)
 
         result[ry][rx][0] = t
@@ -213,40 +168,57 @@ def next_state(result, prev, neighbours_d, counter):
         if t == 0:
             result[ry][rx][1] = 0
         elif t == 1:
-            result[ry][rx][1] = LIFE_1
+            result[ry][rx][1] = LIFE[0]
         elif t == 2:
-            result[ry][rx][1] = LIFE_2
+            result[ry][rx][1] = LIFE[1]
         else:
-            result[ry][rx][1] = LIFE_3
+            result[ry][rx][1] = LIFE[2]
 
 
-if __name__ == "__main__":
-    # board = generate(HEIGHT, WIDTH)
-    board = [[[0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [0, 0], [0, 0], [2, 4], [0, 0], [0, 0]],
-             [[2, 4], [0, 0], [0, 0], [2, 4], [2, 4], [3, 6], [1, 2], [0, 0], [1, 2], [3, 6]],
-             [[2, 4], [0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [1, 2], [2, 4], [0, 0], [0, 0]],
-             [[3, 6], [0, 0], [1, 2], [0, 0], [3, 6], [0, 0], [0, 0], [2, 4], [0, 0], [0, 0]],
-             [[1, 2], [3, 6], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-             [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [0, 0], [2, 4]],
-             [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [2, 4], [3, 6], [0, 0], [0, 0], [0, 0]],
-             [[0, 0], [0, 0], [0, 0], [1, 2], [1, 2], [0, 0], [0, 0], [0, 0], [2, 4], [0, 0]],
-             [[2, 4], [2, 4], [0, 0], [0, 0], [1, 2], [0, 0], [0, 0], [2, 4], [1, 2], [0, 0]],
-             [[0, 0], [0, 0], [1, 2], [0, 0], [1, 2], [0, 0], [1, 2], [0, 0], [0, 0], [0, 0]]]
-    prev = cp.deepcopy(board)
-    board_d = dd(lambda: 0)
-    neighbours_d = dd(lambda: 0)
-    counter = 0
+"""
+def draw(board):  
+    # funkcja wypisująca planszę do konsoli
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j][0] == 0:
+                print(' ', end="")
+            elif board[i][j][0] == 1:
+                print('o', end="")
+            elif board[i][j][0] == 2:
+                print('x', end="")
+            elif board[i][j][0] == 3:
+                print('@', end="")
+        print()
+"""
 
-    while counter < 1000 and not repetition(board_d, board):
-        if counter % REFRESH_IT == 0:
-            draw(board)
-            count(board)
-            time.sleep(0.2)
 
-        clone_board(prev, board)  # ta funkcja modyfikuje prev, zachowujemy tu stan planszy przed nową iteracją
-        next_state(board, prev, neighbours_d, counter)  # ta funkcja generuje następny stan planszy (do zmiennej board)
-        counter += 1
+# if __name__ == "__main__":
+#     # board = generate(HEIGHT, WIDTH)
+#     board = [[[0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [0, 0], [0, 0], [2, 4], [0, 0], [0, 0]],
+#              [[2, 4], [0, 0], [0, 0], [2, 4], [2, 4], [3, 6], [1, 2], [0, 0], [1, 2], [3, 6]],
+#              [[2, 4], [0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [1, 2], [2, 4], [0, 0], [0, 0]],
+#              [[3, 6], [0, 0], [1, 2], [0, 0], [3, 6], [0, 0], [0, 0], [2, 4], [0, 0], [0, 0]],
+#              [[1, 2], [3, 6], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+#              [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [3, 6], [0, 0], [0, 0], [2, 4]],
+#              [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [2, 4], [3, 6], [0, 0], [0, 0], [0, 0]],
+#              [[0, 0], [0, 0], [0, 0], [1, 2], [1, 2], [0, 0], [0, 0], [0, 0], [2, 4], [0, 0]],
+#              [[2, 4], [2, 4], [0, 0], [0, 0], [1, 2], [0, 0], [0, 0], [2, 4], [1, 2], [0, 0]],
+#              [[0, 0], [0, 0], [1, 2], [0, 0], [1, 2], [0, 0], [1, 2], [0, 0], [0, 0], [0, 0]]]
+#     prev = cp.deepcopy(board)
+#     board_d = dd(lambda: 0)
+#     neighbours_d = dd(lambda: 0)
+#     counter = 0
 
-    draw(board)
-    count(board)
-    print("Symulacja się zapętliła." if counter < 1000 else "Koniec.")
+#     while counter < 1000 and not repetition(board_d, board):
+#         if counter % REFRESH_IT == 0:
+#             draw(board)
+#             count(board)
+#             time.sleep(0.2)
+
+#         clone_board(prev, board)  # ta funkcja modyfikuje prev, zachowujemy tu stan planszy przed nową iteracją
+#         next_state(board, prev, neighbours_d, counter)  # ta funkcja generuje następny stan planszy (do zmiennej board)
+#         counter += 1
+
+#     draw(board)
+#     count(board)
+#     print("Symulacja się zapętliła." if counter < 1000 else "Koniec.")
